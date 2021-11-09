e9328a7b6400c73622a2106274a817d0926c41f6
SamHeatfield1199 <asmirnova1199@gmail.com>
2021-11-09 23:36:38 +1000

    did Navbar and preview of Products

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import { AppBar, Badge, IconButton, Toolbar, Typography } from '@material-ui/core'
+import { ShoppingCart } from '@material-ui/icons'
+import React from 'react'
+import useStyles from './styles';
+import logo from '../../assets/commerce.png'
+
+const  Navbar = () =>{
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position='fixed' className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant='h6' className={classes.title} color="inherit">
+                        <img src={logo} alt="logo" height='25px' className={classes.image} />
+                        Commerce.js
+                    </Typography>
+                    <div className={classes.grow}></div>
+                    <div className={classes.button}>
+                        <IconButton aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={2} colors='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
